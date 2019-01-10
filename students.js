d63a3c2514d0b62adb3743936b9b8b7551e63ce3
pigbog <mesteves@gmail.com>
2019-01-10 20:01:31 +0000

    name and age sorting

---
+++ students.js
@@ -0,0 +1,31 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+
+var newArr2= students.sort(function(a,b)
+	{var nameA= a.name;
+	 var nameB= b.name;
+	 
+	 if (nameA < nameB) {
+    return -1;
+  }
+    else if (nameA > nameB) {
+    return 1;
+  }
+  	else if (nameA === nameB){
+  		if (a.age < b.age){
+  			return 1
+  		}
+  		if (b.age > a.age){
+  			return -1
+  		}
+  	};
+});
+
+console.log(newArr2)
+
+	
