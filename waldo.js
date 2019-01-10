2064414e6f474534f0367b92722695180d498742
pigbog <mesteves@gmail.com>
2019-01-10 22:07:32 +0000

    Finally found Waldo.

--- waldo.js
+++ waldo.js
@@ -1,33 +1,15 @@
 // The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {function(names)
-	  arr.forEach(if (arr[i] === "Waldo") {
-	      found();) 
-  
+function findWaldo(arr, found) {
+   arr.forEach(function(names, index)
+   {
+    if (names === "Waldo") {
+      found(index);   // execute callback
+    }
+  })
 }
 
-function actionWhenFound() {
-  console.log("Found him!");
+function actionWhenFound(index) {
+  console.log("Found him at index", index);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-
-
-
-// // The second argument/parameter is expected to be a function
-
-// function checkWaldo(arr){
-// 	if (arr === "Waldo"){ 
-// 		actionWhenFound(arr.indexOf('Waldo')) }
-// }
-// function findWaldo(arr) {
-// 	arr.forEach(checkWaldo)
-// }
-
-// function actionWhenFound(result) {	
-//   console.log("Found him! at", result);
-// }
-
-// findWaldo(["Alice", "Bob", "Waldo", "Winston"]);
-
-// // bleep=["Alice", "Bob", "Waldo", "Winston"]
-// // console.log(bleep.indexOf('Waldo'))
