3ac67de96f35070214381b9022cb150655cae465
pigbog <mesteves@gmail.com>
2019-01-10 19:08:58 +0000

    added pythagorus excercise

--- waldo.js
+++ waldo.js
@@ -1,15 +1,31 @@
-// The second argument/parameter is expected to be a function
-
-function checkWaldo(arr){
-	if (arr === "Waldo"){ actionWhenFound()}
-}
-
-function findWaldo(arr) {
-	arr.forEach(checkWaldo)
+function findWaldo(arr, found) {
+  arr.forEach(function checkWaldo(arr){
+	if (arr === "Waldo"){ 
+		actionWhenFound(arr.indexOf('Waldo')) } {
+  }
 }
 
 function actionWhenFound() {
   console.log("Found him!");
 }
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"]);
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+// // The second argument/parameter is expected to be a function
+
+// function checkWaldo(arr){
+// 	if (arr === "Waldo"){ 
+// 		actionWhenFound(arr.indexOf('Waldo')) }
+// }
+// function findWaldo(arr) {
+// 	arr.forEach(checkWaldo)
+// }
+
+// function actionWhenFound(result) {	
+//   console.log("Found him! at", result);
+// }
+
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"]);
+
+// // bleep=["Alice", "Bob", "Waldo", "Winston"]
+// // console.log(bleep.indexOf('Waldo'))
